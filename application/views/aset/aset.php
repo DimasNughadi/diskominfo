db4a2b0d643d16ab98393e8ada8cfdfb14e1c92d
Dimas Nughadi <dimas19ti@mahasiswa.pcr.ac.id>
2022-04-20 09:43:26 +0700

    base tabel aset

--- application/views/aset/aset.php
+++ application/views/aset/aset.php
@@ -2,59 +2,58 @@
 	<div class="main">
 		<div class="container-fluid">
             <div class="col-lg-10">
-                            <div class="card">
-                                <div class="card-title">
-                                    <h4>Table Striped </h4>
-                                    
-                                </div>
-                                <div class="card-body">
-                                    <div class="table-responsive">
-                                        <table class="table table-striped">
-                                            <thead>
-                                                <tr>
-                                                    <th>No</th>
-                                                    <th>No Aset</th>
-                                                    <th>Nama Aset</th>
-                                                    <th>Jenis</th>
-                                                    <th>Lokasi</th>
-                                                    <th>Owner</th>
-                                                    <th>Subclass</th>
-                                                    <th>Used By</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <th scope="row">1</th>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">2</th>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">3</th>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                    <td>...</td>
-                                                </tr>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
+                <div class="card">
+                    <div class="card-title">
+                        <h4>Table Aset </h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>No Aset</th>
+                                        <th>Nama Aset</th>
+                                        <th>Jenis</th>
+                                        <th>Lokasi</th>
+                                        <th>Owner</th>
+                                        <th>Subclass</th>
+                                        <th>Used By</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                        <td>...</td>
+                                    </tr>
+                            </table>
                         </div>
+                    </div>
+                </div>
+            </div>
