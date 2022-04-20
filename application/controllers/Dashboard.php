dd84c90ac3a5c847e7bcd16fda1eba60a80681c3
Dimas Nughadi <dimas19ti@mahasiswa.pcr.ac.id>
2022-04-20 09:11:14 +0700

    header adn footer layout

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -11,7 +11,9 @@ class Dashboard extends CI_Controller {
     function index()
     {
         $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('layout/header', $data);
         $this->load->view('dashboard', $data);
+        $this->load->view('layout/footer', $data);
 
     }
 }
