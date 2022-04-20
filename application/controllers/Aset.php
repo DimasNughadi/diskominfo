c2e607b3a7358bc62ff60d1d87b9ba6e41185e04
Dimas Nughadi <dimas19ti@mahasiswa.pcr.ac.id>
2022-04-20 09:19:43 +0700

    aset base

---
+++ application/controllers/Aset.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Aset extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index()
+    {
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('layout/header', $data);
+        $this->load->view('aset/aset', $data);
+        $this->load->view('layout/footer', $data);
+
+    }
+}
