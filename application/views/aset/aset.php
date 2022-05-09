fd19c4166ecfa07828e3c7cd1adcc4dda145e809
Dimas Nughadi <dimas19ti@mahasiswa.pcr.ac.id>
2022-05-09 15:14:01 +0700

    taktau

--- application/views/aset/aset.php
+++ application/views/aset/aset.php
@@ -1,49 +1,79 @@
 <div class="content-wrap">
 	<div class="main">
-		<div class="container-fluid">
-            <div class="col-lg-10">
-                <div class="card">
-                    <div class="card-title">
-                        <h4>Table Aset </h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>No Aset</th>
-                                        <th>Nama Aset</th>
-                                        <th>Jenis</th>
-                                        <th>Lokasi</th>
-                                        <th>Owner</th>
-                                        <th>Subclass</th>
-                                        <th>Used By</th>
-                                        <!-- <th>Aksi</th> -->
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php $i = 1; ?>
-                                    <?php foreach ($aset as $us) : ?>
-                                    <tr>
-                                        <th scope="row"><?= $i; ?></th>
-                                        <td><?= $us['no_aset']; ?></td>
-                                        <td><?= $us['nama_aset']; ?></td>
-                                        <td><?= $us['jenis_aset']; ?></td>
-                                        <td><?= $us['owner_aset']; ?></td>
-                                        <td><?= $us['lokasi_aset']; ?></td>
-                                        <td><?= $us['subclass_aset']; ?></td>
-                                        <td><?= $us['used_by']; ?></td>
-                                        <!-- <td>
-                                            <a href="<?= base_url('Aset/detail') . $us[id_aset]; ?>"></a>
-                                            <a href="<?= base_url('Aset/detail') . $us[id_aset]; ?>"></a>
-                                            <a href="<?= base_url('Aset/detail') . $us[id_aset]; ?>"></a>
-                                        </td> -->
-                                    </tr>
-                                    <?php $i++; ?>
-                                    <?php endforeach; ?>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
+		<!-- Main content -->
+		<section class="content">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-12">
+						<div class="card">
+							<?= $this->session->flashdata('message');?>
+							<!-- /.card-header -->
+							<div class="card-body">
+								<table id_aset="example1" class="table table-bordered table-striped">
+									<thead>
+										<tr>
+											<th>No</th>
+											<th>No Aset</th>
+											<th>Nama Aset</th>
+											<th>Jenis</th>
+											<th>Lokasi</th>
+											<th>Owner</th>
+											<th>Subclass</th>
+											<th>Used By</th>
+											<th>Aksi</th>
+										</tr>
+									</thead>
+									<tbody>
+										<?php $i = 1; ?>
+										<?php foreach ($aset as $us) : ?>
+										<tr>
+											<td><?= $i; ?></td>
+											<td><?= $us['no_aset']; ?></td>
+											<td><?= $us['nama_aset']; ?></td>
+											<td><?= $us['jenis_aset']; ?></td>
+											<td><?= $us['owner_aset']; ?></td>
+											<td><?= $us['lokasi_aset']; ?></td>
+											<td><?= $us['subclass_aset']; ?></td>
+											<td><?= $us['used_by']; ?></td>
+											<td>
+												<button type="button" class="btn btn-danger btn-sm"
+													<?php echo $us['id_aset']; ?>>
+													<i class="ti-trash"></i>
+												</button>
+												<button type="button" class="btn btn-primary btn-sm"
+													<?php echo $us['id_aset']; ?>>
+													<i class="ti-info-alt"></i>
+												</button>
+											</td>
+										</tr>
+										<?php $i++; ?>
+										<?php endforeach; ?>
+									</tbody>
+									<tfoot>
+										<tr>
+											<th>No</th>
+											<th>No Aset</th>
+											<th>Nama Aset</th>
+											<th>Jenis</th>
+											<th>Lokasi</th>
+											<th>Owner</th>
+											<th>Subclass</th>
+											<th>Used By</th>
+											<th>aksi</th>
+										</tr>
+									</tfoot>
+								</table>
+							</div>
+							<!-- /.card-body -->
+						</div>
+						<!-- /.card -->
+					</div>
+					<!-- /.col -->
+				</div>
+				<!-- /.row -->
+			</div>
+			<!-- /.container-fluid -->
+		</section>
+		<!-- /.content -->
+	</div>
+	<!-- /.content-wrapper -->
