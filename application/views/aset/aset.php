f12ca6c13132fcabcc0c89d9d1d8d30a9e757a95
rezafanfanayulius12@gmail.com <rezafanfanayulius12@gmail.com>
2022-05-17 10:56:39 +0700

    Update cotroller aset

--- application/views/aset/aset.php
+++ application/views/aset/aset.php
@@ -14,7 +14,7 @@
 			<div id="main-content">
 				<div class="row">
 					<div class="col-lg-12">
-					<div class="col-md-6"><a href="<?= base_url(); ?>Aset/tambah" class="btn btn-info mb-2">Tambah Aset</a></div>
+						<div class="col-md-6"><a href="<?= base_url(); ?>Aset/tambah" class="btn btn-info mb-2">Tambah Aset</a></div>
 						<div class="card">
 							<div class="jsgrid-table-panel">
 								<?= $this->session->flashdata('message'); ?>
@@ -47,9 +47,63 @@
 													<td><?= $us['subclass_aset']; ?></td>
 													<td><?= $us['used_by']; ?></td>
 													<td>
-														<button type="button" class="btn btn-primary btn-sm" <?php echo $us['id_aset']; ?>>
-															<i class="ti-info-alt"></i>
-														</button>
+
+														<!-- Trigger Detail -->
+														<a href="" class="btn btn-info btn-sm" data-toggle="modal" data-target="#modal-default<?php echo $us['id_aset'] ?>">Detail</a>
+														<!-- Modal -->
+														<div class="modal fade" id="modal-default<?php echo $us['id_aset'] ?>">
+															<div class="modal-dialog">
+																<div class="modal-content">
+																	<div class="modal-header">
+																		<h4 class="modal-title">Detail Pesanan</h4>
+																		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+																			<span aria-hidden="true">&times;</span>
+																		</button>
+																	</div>
+																	<div class="modal-body">
+																		<!-- Profile Image -->
+																		<div class="card card-primary card-outline">
+																			<div class="card-body box-profile">
+
+																				<h3 class="profile-username text-center"><?php echo $us['no_aset']; ?></h3>
+
+																				<p class="text-muted text-center">
+																					<?php echo $us['nama_aset']; ?></h3>
+																				</p>
+
+																				<ul class="list-group list-group-unbordered mb-3">
+																					<li class="list-group-item">
+																						<b>Nama</b>
+																					</li>
+																					<li class="list-group-item">
+																						<b>Nama Paket</b>
+																						<a class="float-right">
+
+																						</a>
+																					</li>
+																					<li class="list-group-item">
+																						<b>Harga</b>
+
+																						</a>
+																					</li>
+																					<li class="list-group-item">
+																						<b>No HP</b>
+																					</li>
+
+																				</ul>
+																			</div>
+																			<!-- /.card-body -->
+																		</div>
+																	</div>
+																	<div class="modal-footer justify-content-between">
+																		<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+																	</div>
+																</div>
+																<!-- /.modal-content -->
+															</div>
+															<!-- /.modal-dialog -->
+														</div>
+														<!-- /.modal -->
 														<button type="button" class="btn btn-success	 btn-sm" <?php echo $us['id_aset']; ?>>
 															<i class="ti-pencil"></i>
 														</button>
