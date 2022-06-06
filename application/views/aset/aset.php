372feededea191c7c45db6d1786e93ed0c8d4b18
rezafanfanayulius12@gmail.com <rezafanfanayulius12@gmail.com>
2022-06-06 14:38:17 +0700

    Updating view aset

--- application/views/aset/aset.php
+++ application/views/aset/aset.php
@@ -179,26 +179,27 @@
 															</div>
 															<!-- /.modal -->
 
-														<!-- Trigger Edit -->
-														<button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#modal-success<?php echo $us['id_aset'];?>">
-															<i class="ti-pencil-alt"></i>
-														</button>
-														<!-- Modal -->
-														<div class="modal fade" id="modal-success<?php echo $us['id_aset'];?>">
-															<div class="modal-dialog">
-																<div class="modal-content bg-success">
-																	<div class="modal-header">
-																		<h4 class="text-light">Edit Data Aset</h4>
-																		<button type="button" class="btn" data-dismiss="modal" aria-label="Close">
-																			<i class="fa fa-close"></i>
-																		</button>
-																	</div>
-																	<div class="modal-body">
-																		<p class="text-light">Anda yakin ingin mengubah data ini&hellip; ?</p>
-																	</div>
-																	<div class="modal-footer justify-content-between">
-																		<button type="button" class="btn btn-outline-light" data-dismiss="modal">Tutup</button>
-																		<a href="<?= base_url('aset/edit/') . $us['id_aset']; ?>" class="btn btn-outline-light">Ubah</a>
+															<!-- Trigger Edit -->
+															<button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#modal-success<?php echo $us['id_aset']; ?>">
+																<i class="ti-pencil-alt"></i>
+															</button>
+															<!-- Modal -->
+															<div class="modal fade" id="modal-success<?php echo $us['id_aset']; ?>">
+																<div class="modal-dialog">
+																	<div class="modal-content bg-success">
+																		<div class="modal-header">
+																			<h4 class="text-light">Edit Data</h4>
+																			<button type="button" class="btn" data-dismiss="modal" aria-label="Close">
+																				<i class="fa fa-close"></i>
+																			</button>
+																		</div>
+																		<div class="modal-body">
+																			<p class="text-light">Anda yakin ingin mengubah data ini&hellip; ?</p>
+																		</div>
+																		<div class="modal-footer justify-content-between">
+																			<button type="button" class="btn btn-outline-light" data-dismiss="modal">Close</button>
+																			<a href="<?= base_url('aset/edit/') . $us['id_aset']; ?>" class="btn btn-outline-light">Ubah</a>
+																		</div>
 																	</div>
 																	<!-- /.modal-content -->
 																</div>
@@ -211,22 +212,23 @@
 																<i class="ti-trash"></i>
 															</button>
 
-														<!-- Modal -->
-														<div class="modal fade" id="modal-danger<?php echo $us['id_aset']; ?>">
-															<div class="modal-dialog">
-																<div class="modal-content bg-danger">
-																	<div class="modal-header">
-																		<h4 class="text-light">Hapus Data Aset</h4>
-																		<button type="button" class="btn" data-dismiss="modal" aria-label="Close">
-																			<i class="fa fa-close"></i>
-																		</button>
-																	</div>
-																	<div class="modal-body">
-																		<p class="card-title text-light">Anda yakin akan menghapus data ini&hellip; ?</p>
-																	</div>
-																	<div class="modal-footer justify-content-between">
-																		<button type="button" class="btn btn-outline-light" data-dismiss="modal">Tutup</button>
-																		<a href="<?= base_url('Aset/hapus/') . $us['id_aset']; ?>" class="btn btn-outline-light">Hapus</a>
+															<!-- Modal -->
+															<div class="modal fade" id="modal-danger<?php echo $us['id_aset']; ?>">
+																<div class="modal-dialog">
+																	<div class="modal-content bg-danger">
+																		<div class="modal-header">
+																			<h4 class="text-light">Hapus Data Aset</h4>
+																			<button type="button" class="btn" data-dismiss="modal" aria-label="Close">
+																				<i class="fa fa-close"></i>
+																			</button>
+																		</div>
+																		<div class="modal-body">
+																			<p class="card-title text-light">Anda yakin akan menghapus data ini&hellip; ?</p>
+																		</div>
+																		<div class="modal-footer justify-content-between">
+																			<button type="button" class="btn btn-outline-light" data-dismiss="modal">Tutup</button>
+																			<a href="<?= base_url('Aset/hapus/') . $us['id_aset']; ?>" class="btn btn-outline-light">Simpan Perubahan</a>
+																		</div>
 																	</div>
 																	<!-- /.modal-content -->
 																</div>
