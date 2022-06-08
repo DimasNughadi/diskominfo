faf7aa1569d34f42d6132afc5b5ba88184a51314
hadirangkuti <hadirangkuti7175@gmail.com>
2022-06-08 14:38:58 +0700

    crud bidang

--- application/controllers/Bidang.php
+++ application/controllers/Bidang.php
@@ -44,7 +44,7 @@ class Bidang extends CI_Controller
             $this->Bidang_model->insert($data);
             $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data
                                                 Bidang Berhasil Ditambah!</div>');
-            redirect('Dashboard');
+            redirect('Bidang');
         }
     }
     public function hapus($id)
@@ -63,7 +63,7 @@ class Bidang extends CI_Controller
     {
         $data['judul'] = "Edit Data Bidang";
         $data['bidang'] = $this->Bidang_model->getById($id);
-        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
 
         $this->form_validation->set_rules('nama_bidang', 'Nama Bidang', 'required', [
             'required' => 'Nama Bidang Wajib di isi'
@@ -74,10 +74,11 @@ class Bidang extends CI_Controller
 
         if ($this->form_validation->run() == false) {
             $this->load->view("layout/header", $data);
-            $this->load->view("bidang/vw_ubah_bidang", $data);
+            $this->load->view("bidang/vw_edit_bidang", $data);
             $this->load->view("layout/footer");
         } else {
             $data = [
+                'id_bidang' => $this->input->post('id_bidang'),
                 'nama_bidang' => $this->input->post('nama_bidang'),
                 'lokasi' => $this->input->post('lokasi'),
             ];
