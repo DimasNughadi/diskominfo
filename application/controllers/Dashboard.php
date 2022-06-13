a91a30d51397d16b9b26a5c546e5cd2349310ebf
Dimas Nughadi <dimas19ti@mahasiswa.pcr.ac.id>
2022-06-13 15:49:50 +0700

    pie chart pertama

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -9,6 +9,7 @@ class Dashboard extends CI_Controller {
         check_not_login();
         $this->load->model('Aset_model');
         $this->load->model('User_model');
+        $this->load->model('Resiko_model');
     }
 
     function index()
@@ -17,6 +18,11 @@ class Dashboard extends CI_Controller {
         $data['judul'] = "Dashboard";
         $data['aset'] = $this->Aset_model->getCount();
         $data['usercount'] = $this->User_model->getCount();
+        $data['sangatrendah'] = $this->Resiko_model->getCountSR();
+        $data['rendah'] = $this->Resiko_model->getCountR();
+        $data['sedang'] = $this->Resiko_model->getCountS();
+        $data['tinggi'] = $this->Resiko_model->getCountT();
+        $data['sangattinggi'] = $this->Resiko_model->getCountST();
         $this->load->view('layout/header', $data);
         $this->load->view('dashboard', $data);
         $this->load->view('layout/footer', $data);
