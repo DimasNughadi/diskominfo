5306f9f095357d42937dfe09bd229e0c00d6b503
Dimas Nughadi <dimas19ti@mahasiswa.pcr.ac.id>
2022-06-15 00:27:47 +0700

    rapiin sikit

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -31,68 +31,6 @@ class Auth extends CI_Controller
         }
     }
 
-    // function registrasi()
-    // {
-    //     if ($this->session->userdata('email')) {
-    //         redirect('welcome');
-    //     }
-    //     $this->form_validation->set_rules('nama', 'Nama', 'required|trim', [
-    //         'required' => 'Nama Wajib di isi'
-    //     ]);
-    //     $this->form_validation->set_rules('jenis_kelamin', 'Jenis Kelamin', 'required', [
-    //         'required' => 'Jenis Kelamin Wajib di isi'
-    //     ]);
-    //     $this->form_validation->set_rules('alamat', 'Alamat', 'required', [
-    //         'required' => 'Alamat Wajib di isi'
-    //     ]);
-    //     $this->form_validation->set_rules('kota', 'Kota', 'required', [
-    //         'required' => 'Kota Wajib di isi'
-    //     ]);
-    //     $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
-    //         'is_unique' => 'Email ini sudah terdaftar!',
-    //         'valid_email' => 'Email Harus Valid',
-    //         'required' => 'Email Wajib di isi'
-    //     ]);
-    //     $this->form_validation->set_rules('no_hp', 'NoHP', 'required|numeric', [
-    //         'numeric' => 'No HP Harus Angka!',
-    //         'required' => 'No HP Wajib di isi'
-    //     ]);
-    //     $this->form_validation->set_rules(
-    //         'password1',
-    //         'Password',
-    //         'required|trim|min_length[5]|matches[password2]',
-    //         [
-    //             'matches' => 'Password Tidak Sama',
-    //             'min_length' => 'Password Terlalu Pendek',
-    //             'required' => 'Password harus diisi'
-    //         ]
-    //     );
-    //     $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
-    //     if ($this->form_validation->run() == false) {
-    //         $data['title'] = 'Registration';
-    //         $this->load->view('layout/auth_header', $data);
-    //         $this->load->view('auth/registrasi');
-    //         $this->load->view('layout/auth_footer');
-    //     } else {
-    //         $data = [
-    //             'nama' => htmlspecialchars($this->input->post('nama', true)),
-    //             'jenis_kelamin' => htmlspecialchars($this->input->post('jenis_kelamin', true)),
-    //             'alamat' => htmlspecialchars($this->input->post('alamat', true)),
-    //             'kota' => htmlspecialchars($this->input->post('kota', true)),
-    //             'email' => htmlspecialchars($this->input->post('email', true)),
-    //             'no_hp' => htmlspecialchars($this->input->post('no_hp', true)),
-    //             'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
-    //             'gambar' => 'default.jpg',
-    //             'role' => "User",
-    //             'status' => "Active",
-    //             'date_created' => time()
-    //         ];
-    //         $this->userrole->insert($data);
-    //         $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Selamat!  Akunmu telah berhasil terdaftar, Silahkan Login! </div>');
-    //         redirect('auth');
-    //     }
-    // }
-
     function cek_login()
     {
         $username = $this->input->post('username');
