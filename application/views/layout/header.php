7d5877dedf7bdc7752fb4957944c418704cb4c43
Dimas Nughadi <dimas19ti@mahasiswa.pcr.ac.id>
2022-07-04 09:49:09 +0700

    modal logout

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -84,13 +84,35 @@
 						</ul>
 					</li>
 					<li><a href="<?= site_url('account') ?>"><i class="ti-id-badge"></i> Account</a></li>
-					<li><a href="<?= site_url('auth/logout') ?>"><i class="ti-close"></i> Logout</a></li>
+					<li><a data-toggle="modal" data-target="#modal-default" selected="false"> <i class="ti-close"></i> Logout</a></li>
 				</ul>
 			</div>
 		</div>
 	</div>
 	<!-- /# sidebar -->
-
+	
+	<div class="modal fade" id="modal-default">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title">Konfirmasi</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<p>Apakah anda yakin ingin logout&hellip; ?</p>
+				</div>
+				<div class="modal-footer justify-content-between">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
+					<a href="<?= site_url('auth/logout')?>" class="btn btn-primary">Logout</a>
+				</div>
+			</div>
+			<!-- /.modal-content -->
+		</div>
+		<!-- /.modal-dialog -->
+	</div>
+	<!-- /.modal -->
 	<div class="header">
 		<div class="container-fluid">
 			<div class="row">
